d6025c57c18be2d8fa36c80e2c24ebbb2949bd51
Gur Keinan <Gur@gurk7.com>
2020-04-02 18:29:41 +0300

    added success only handler

--- src/_helpers/handle-response.js
+++ src/_helpers/handle-response.js
@@ -3,7 +3,7 @@ import { authenticationService } from '@/_services';
 export function handleResponse(response) {
     return response.text().then(text => {
         const data = text && JSON.parse(text);
-        if (!response.ok) {
+        if (!response.ok || data.success === false) {
             if ([401, 403].indexOf(response.status) !== -1) {
                 // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
                 authenticationService.logout();
